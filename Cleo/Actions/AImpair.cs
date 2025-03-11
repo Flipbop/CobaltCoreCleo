2b184149bf7054d018a6f0502babd081e70c7b10
Flipbop <151550611+Flipbop@users.noreply.github.com>
2025-03-11 00:51:45 -0600

    11
    Improve A/B and Impair *likely* work, but they don't remove when played as of now.

---
+++ Cleo/Actions/AImpair.cs
@@ -0,0 +1,45 @@
+﻿using FSPRO;
+using Nickel;
+using System.Collections.Generic;
+
+
+namespace Flipbop.Cleo;
+
+public sealed class AImpair : DynamicWidthCardAction
+{
+	public required int Amount;
+
+	public override void Begin(G g, State s, Combat c)
+	{
+		base.Begin(g, s, c);
+		int index = c.hand.Count -1;
+		while (Amount > 0)
+		{
+			if (index < 0)
+			{
+				break;
+			}
+			if (c.hand[index].upgrade != Upgrade.None)
+			{
+				c.hand[index].upgrade = Upgrade.None;
+				Amount--;
+				Audio.Play(Event.CardHandling);
+			}
+			index--;
+		}
+	}
+
+	public override Icon? GetIcon(State s)
+		=> new(ModEntry.Instance.ImprovedIcon.Sprite, Amount == -1 ? null : Amount, Colors.textMain);
+
+	public override List<Tooltip> GetTooltips(State s)
+		=> [
+			new GlossaryTooltip($"action.{ModEntry.Instance.Package.Manifest.UniqueName}::Impair")
+			{
+				Icon = ModEntry.Instance.ImprovedIcon.Sprite,
+				TitleColor = Colors.action,
+				Title = ModEntry.Instance.Localizations.Localize(["action", "Impair", "name"]),
+				Description = ModEntry.Instance.Localizations.Localize(["action", "Impair", "description"])
+			}
+		];
+}
