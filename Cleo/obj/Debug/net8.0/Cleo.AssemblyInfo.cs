592b6cdc7edf6710f51fcbe47cf0aca2604d4a49
Flipbop <151550611+Flipbop@users.noreply.github.com>
2025-04-08 23:37:33 -0600

    0.9.0
    okay NOW it's ready

--- Cleo/obj/Debug/net8.0/Cleo.AssemblyInfo.cs
+++ Cleo/obj/Debug/net8.0/Cleo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("0.9.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.9.0+fdf1d040b555bd1fe9d2a48d21fb41174640eca9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("0.9.0+780c980285dfbcc015ac6a1310ff85e7b5111dff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("0.9.0.0")]
