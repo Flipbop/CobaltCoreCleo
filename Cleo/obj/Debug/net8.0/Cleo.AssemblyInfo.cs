8c22de3c69fee0b0340bab32a1010f69e77b7e59
Flipbop <151550611+Flipbop@users.noreply.github.com>
2025-05-12 22:28:08 -0600

    1.0.1

--- Cleo/obj/Debug/net8.0/Cleo.AssemblyInfo.cs
+++ Cleo/obj/Debug/net8.0/Cleo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c1ce9a161cdcaa7c9c4085903afd0f87ab05240")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+347aa20ae617900724b2f7e6f00f28aad2feac9a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Cleo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
